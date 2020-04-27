a0d2d9b52021cda22bd18817448ff65a82e15d04
Saed Yousef <saed.alzaben@gmail.com>
2020-04-27 23:13:05 +0300

    Update Triangle.php

--- Triangle.php
+++ Triangle.php
@@ -1,19 +1,9 @@
 <?php
-/**
-* @author Saed Yousef
-* Class DrawTriangle to handle the methodglogy of drawing the triangle
-*/
+
 class Triangle{
 
-	/**
-	* @author Saed Yousef
-	* @param $rows
-	* @param $repeat
-	* @return triangle of stars
-	*/
 	public function draw($rows, $repeat)
 	{	
-		// Check the number of rows and repetition
 		if($rows <= 0 || $repeat <= 0)
 		{
 			echo "The number of rows and reaption must be more than 0";
